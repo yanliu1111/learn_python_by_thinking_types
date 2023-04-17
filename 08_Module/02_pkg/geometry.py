50dec70294bbb857937da76f2c5b16a4d07b95f9
yanliu1111 <liuyan0504@gmail.com>
2023-04-17 16:40:53 -0600

    feat: module done

---
+++ 08_Module/02_pkg/geometry.py
@@ -0,0 +1,11 @@
+from shapes.circle import Circle
+from shapes.rectangle import Rectangle
+from shapes.square import Square
+
+c = Circle(5)
+r = Rectangle(5, 10)
+s = Square(5)
+
+print(c)
+print(r)
+print(s)
---
+++ 08_Module/02_pkg/shapes/square.py
@@ -0,0 +1,5 @@
+class Square:
+    def __init__(self, side: float)-> None:
+        self.side = side
+    def __str__(self) -> str:
+        return f"Square: {self.side} x {self.side}"
